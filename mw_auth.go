be5182bd603470ff2410660b8e2a3f7013cebfd6
lcb <liuchengbin@laoyuegou.com>
2020-03-29 13:02:57 +0800

    up

--- mw_auth.go
+++ mw_auth.go
@@ -10,24 +10,16 @@ import (
 // AuthMiddleware 用户授权中间件
 func AuthMiddleware(skipper ...SkipperFunc) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		var userID string
 		if t := GetToken(c); t != "" {
-			claims, err := jwtplus.ParseToken(t)
+			_, err := jwtplus.ParseToken(t)
 			if err != nil {
 				ResError(c, errors.New("无效的token"))
 				return
 			}
-			userID = claims.UserId
-		}
-		if userID != "" {
-			c.Set(UserIDKey, userID)
 		}
 		if len(skipper) > 0 && skipper[0](c) {
 			c.Next()
 			return
-		}
-		if userID == "" {
-			ResError(c, errors.New("无权限"))
 		}
 	}
 }
